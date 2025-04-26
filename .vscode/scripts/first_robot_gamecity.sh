2a47fe183f6d3fe9112a3b6cf4c630b8760d7f30
EpicSlayer12 <pedroaguileragomez@gmail.com>
2025-04-26 18:48:58 +0800

    Initial commit

---
+++ .vscode/scripts/first_robot_gamecity.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash .vscode/scripts/build.sh
+
+source install/setup.bash
+ros2 launch my_first_robot gazebo.launch.py
---
+++ .vscode/scripts/run_freecad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
+
+echo "Starting FreeCAD, please wait ..."
+export ROS_DISTRO=jazzy
+/home/ubuntu/FreeCAD.AppImage --appimage-extract-and-run --module-path ${PYTHONPATH//:/' --module-path '} 
